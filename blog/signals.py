08d503d6becaed45182df8efc07fa73f1eedcb8f
satyendragautam90 <satyendragautam901@gmail.com>
2025-08-11 11:32:40 +0530

    Models signals: pre_save, post_save etc..

--- blog/signals.py
+++ blog/signals.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.signals import user_logged_in,user_logged_out,user_login_failed
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-
+from django.db.models.signals import pre_init, post_init, pre_save, post_save, pre_delete,post_delete
+from .models import Blog
 # user logged signal
 
 @receiver(user_logged_in, sender = User)# you can also use connect method but receive is more handy
@@ -35,3 +36,73 @@ def logout_signal(sender, credentials, request, **kwargs):
     print("Request: ", request)
     print("Credentials: ", credentials)
     print(f'kwargs: {kwargs}')
+
+# models signals
+@receiver(pre_save, sender=Blog)  # pre_save signal, sender will be the model to listen to (Blog in this case)
+def presave_signal(sender, instance, raw, using, update_fields, **kwargs):
+    print("**************")
+    print("Pre Save Signal")
+    print("Sender: ", sender)  # The model class (Blog) that triggered the signal
+    print("Instance: ", instance)  # The actual model instance (e.g., the Blog object being saved)
+    print("Raw: ", raw)  # Boolean indicating whether the model is being saved in raw form (default is False)
+    print("Using: ", using)  # The database alias being used (e.g., 'default' if no custom DB is specified)
+    print("Update fields: ", update_fields)  # List of fields being updated, or None if saving all fields
+    print(f'kwargs: {kwargs}')  # Any additional keyword arguments passed to the signal (usually empty)
+
+@receiver(post_save, sender = Blog) # post save signal
+def postsave_signal(sender, instance,created, raw, using, update_fields, **kwargs):
+    if created: # if new record then this will run otherwise next one
+        print("**************")
+        print("Post Save Signal")
+        print("Sender: ", sender) 
+        print("Instance: ", instance)  
+        print("Raw: ", raw)  
+        print("Using: ", using) 
+        print("Update fields: ", update_fields)  
+        print("Created: ", created) # if new instance is created then true otherwise false
+        print(f'kwargs: {kwargs}')  
+
+    print("**************")
+    print("Post Save Signal")
+    print("Sender: ", sender) 
+    print("Instance: ", instance)  
+    print("Raw: ", raw)  
+    print("Using: ", using) 
+    print("Update fields: ", update_fields)  
+    print("Created: ",created)
+    print("Updated record ") 
+    print(f'kwargs: {kwargs}') 
+
+# delete signal
+@receiver(pre_delete, sender = Blog) # pre delete signal
+def predelete_singal(sender, instance, **kwargs):
+    print("**************")
+    print("Pre delete Signal")
+    print("Sender: ", sender) 
+    print("Instance: ", instance)
+    print(f'kwargs: {kwargs}') 
+
+@receiver(post_delete, sender = Blog) # post delete signal
+def postdelete_singal(sender, instance, **kwargs):
+    print("**************")
+    print("Post delete Signal")
+    print("Sender: ", sender) 
+    print("Instance: ", instance)
+    print(f'kwargs: {kwargs}') 
+
+# pre and posts init signals
+@receiver(pre_init, sender = Blog)
+def preinit_signal(sender, args, **kwargs):
+    print("**************")
+    print("Pre Init Signal")
+    print("Sender: ", sender) 
+    print("Args: ", args)
+    print(f'kwargs: {kwargs}') 
+
+@receiver(post_init, sender = Blog)
+def postinit_signal(sender, *args, **kwargs):
+    print("**************")
+    print("Post Init Signal")
+    print("Sender: ", sender) 
+    print(f'Args: {args}')
+    print(f'kwargs: {kwargs}') 
